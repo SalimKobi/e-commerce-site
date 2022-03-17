ebfbf02d69162c7413ac49becfa380b634fdb4cb
SalimKobi <salimkobi9@gmail.com>
2022-03-17 21:53:14 +0100

    completed the unique Features section with it's responsiveness

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ import body from "./components/body.vue";
           </div>
         </div>
         <div class="relative">
-          <ul class="flex justify-evenly">
+          <ul class="flex justify-evenly items-center">
             <li class="hidden sm:flex">
               English<img
                 class="w-[13px] sm:w-auto sm:pl-[3px]"
@@ -50,14 +50,14 @@ import body from "./components/body.vue";
             <li class="hidden sm:flex sm:pl-[31px]">
               Wishlist<img
                 class="sm:pl-[3px]"
-                src="./assets/img/uil_heart-alt.svg"
+                src="./assets/img/uil_heart-white.svg"
                 alt=""
               />
             </li>
             <li class="ml-[10px] sm:pl-[31px]">
               <img
-                class="sm:pl-[3px]"
-                src="./assets/img/fluent_cart-24-regular.svg"
+                class="sm:pl-[3px] w-[30px]"
+                src="./assets/img/fluent_cart-24-white.svg"
                 alt=""
               />
             </li>
@@ -212,8 +212,10 @@ import body from "./components/body.vue";
                 src="./assets/img/image_3.png"
                 alt=""
               />
-              <ul class="flex items-center absolute -mt-[15rem]">
-                <li><img src="./assets/img/Group 28.svg" alt="" /></li>
+              <ul class="flex items-center absolute -mt-[14rem] ml-[11px]">
+                <li>
+                  <img src="./assets/img/fluent_cart-24-regular.svg" alt="" />
+                </li>
                 <li class="ml-[10px]">
                   <img src="./assets/img/uil_heart-alt.svg" alt="" />
                 </li>
@@ -231,8 +233,8 @@ import body from "./components/body.vue";
                   py-[8px]
                   px-[13px]
                   text-[12px]
-                  ml-[8rem]
-                  sm:ml-[6rem]
+                  ml-[7rem]
+                  sm:ml-[70px]
                 "
               >
                 View Details
@@ -311,19 +313,316 @@ import body from "./components/body.vue";
       </div>
     </div>
   </section>
-  <section>
-    <div class="container mx-auto">
+
+  <!-- Latest products section  -->
+  <section class="px-[30px]">
+    <div class="px-[30px] container mx-auto">
       <p class="text-center text-[20px] sm:text-[42px]">Latest Products</p>
-      <ul class="flex justify-center mt-[19px] space-x-[58px]">
+      <ul
+        class="
+          flex flex-wrap
+          justify-center
+          mt-[19px]
+          space-x-[13px]
+          sm:space-x-[58px]
+          text-[#151875]
+          mb-[58px]
+        "
+      >
         <li class="text-[#FB2E86] underline underline-offset-4">New Arrival</li>
         <li>Best Seller</li>
         <li>Featured</li>
         <li>Special offer</li>
       </ul>
     </div>
+    <div></div>
+    <!-- first product -->
+    <div
+      class="
+        grid grid-cols-1
+        md:grid-cols-2
+        lg:grid-cols-3
+        gap-10
+        md:max-w-[70vw]
+        mx-auto
+      "
+    >
+      <div>
+        <div class="flex bg-gray-100">
+          <img
+            class="h-[19rem] w-full object-scale-down object-center"
+            src="./assets/img/image 1166.png"
+            alt=""
+          />
+        </div>
+        <div class="text-[#151875]">
+          <ul class="flex justify-between">
+            <li class="text-[16px]">Comfort Handy Craft</li>
+            <li>
+              <ul class="flex space-x-[5px] items-center">
+                <li class="text-[14px]">$42.00</li>
+                <li class="text-[12px] text-[#FB2448] line-through">$65.00</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- second product -->
+      <div>
+        <div class="flex">
+          <img
+            class="h-[19rem] w-full object-scale-down object-center"
+            src="./assets/img/image 15.png"
+            alt=""
+          />
+          <li class="absolute list-none">
+            <img src="./assets/img/Group 27.svg" alt="" />
+          </li>
+          <ul class="flex-cols absolute mt-[12rem] pl-[20px] items-center">
+            <li>
+              <img src="./assets/img/fluent_cart-24-regular.svg" alt="" />
+            </li>
+            <li class="ml-[10px]">
+              <img src="./assets/img/uil_heart-alt.svg" alt="" />
+            </li>
+            <li class="ml-[16px]">
+              <img src="./assets/img/uil_search-plus.svg" alt="" />
+            </li>
+          </ul>
+        </div>
+        <div class="text-[#151875]">
+          <ul class="flex justify-between">
+            <li class="text-[16px]">Comfort Handy Craft</li>
+            <li>
+              <ul class="flex space-x-[5px] items-center">
+                <li class="text-[14px]">$42.00</li>
+                <li class="text-[12px] text-[#FB2448] line-through">$65.00</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- third product  -->
+      <div>
+        <div class="flex bg-gray-100">
+          <img
+            class="h-[19rem] w-full object-scale-down object-center"
+            src="./assets/img/image 1168.png"
+            alt=""
+          />
+        </div>
+        <div class="text-[#151875]">
+          <ul class="flex justify-between">
+            <li class="text-[16px]">Comfort Handy Craft</li>
+            <li>
+              <ul class="flex space-x-[5px] items-center">
+                <li class="text-[14px]">$42.00</li>
+                <li class="text-[12px] text-[#FB2448] line-through">$65.00</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- fourth product -->
+      <div>
+        <div class="flex bg-gray-100">
+          <img
+            class="h-[19rem] w-full object-scale-down object-center"
+            src="./assets/img/image 23.png"
+            alt=""
+          />
+        </div>
+        <div class="text-[#151875]">
+          <ul class="flex justify-between">
+            <li class="text-[16px]">Comfort Handy Craft</li>
+            <li>
+              <ul class="flex space-x-[5px] items-center">
+                <li class="text-[14px]">$42.00</li>
+                <li class="text-[12px] text-[#FB2448] line-through">$65.00</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- fifth product  -->
+      <div>
+        <div class="flex bg-gray-100">
+          <img
+            class="h-[19rem] w-full object-scale-down object-center"
+            src="./assets/img/image_32a.png"
+            alt=""
+          />
+        </div>
+        <div class="text-[#151875]">
+          <ul class="flex justify-between">
+            <li class="text-[16px]">Comfort Handy Craft</li>
+            <li>
+              <ul class="flex space-x-[5px] items-center">
+                <li class="text-[14px]">$42.00</li>
+                <li class="text-[12px] text-[#FB2448] line-through">$65.00</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- sixth product -->
+      <div>
+        <div class="flex bg-gray-100">
+          <img
+            class="h-[19rem] w-full object-scale-down object-center"
+            src="./assets/img/image 3.png"
+            alt=""
+          />
+        </div>
+        <div class="text-[#151875]">
+          <ul class="flex justify-between">
+            <li class="text-[16px]">Comfort Handy Craft</li>
+            <li>
+              <ul class="flex space-x-[5px] items-center">
+                <li class="text-[14px]">$42.00</li>
+                <li class="text-[12px] text-[#FB2448] line-through">$65.00</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="px-[30px]">
+    <p class="text-center text-[20px] sm:text-[42px] py-[55px]">
+      What Shopex Offer!
+    </p>
+    <div
+      class="
+        grid grid-cols-1
+        md:grid-cols-2
+        lg:grid-cols-4
+        gap-10
+        md:max-w-[70vw]
+        mx-auto
+      "
+    >
+      <div
+        class="
+          shadow-[0px_0px_25px_rgba(0,_0,_0,_0.1)]
+          sm:px-[12px]
+          px-[50px]
+          py-[20px]
+        "
+      >
+        <img
+          class="mt-[27px] mx-auto mb-[22px]"
+          src="./assets/img/free-delivery 1.png"
+          alt=""
+        />
+        <p class="mb-[20px] text-center font-bold">24/7 support</p>
+        <p class="text-[12px] text-center">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto minus
+          facilis magni autem?
+        </p>
+      </div>
+      <div
+        class="
+          shadow-[0px_0px_25px_rgba(0,_0,_0,_0.1)]
+          sm:px-[12px]
+          px-[50px]
+          py-[20px]
+        "
+      >
+        <img
+          class="mt-[27px] mx-auto mb-[22px]"
+          src="./assets/img/cashback 1.png"
+          alt=""
+        />
+        <p class="mb-[20px] text-center font-bold">24/7 support</p>
+        <p class="text-[12px] text-center">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto minus
+          facilis magni autem?
+        </p>
+      </div>
+      <div
+        class="
+          shadow-[0px_0px_25px_rgba(0,_0,_0,_0.1)]
+          sm:px-[12px]
+          px-[50px]
+          py-[20px]
+        "
+      >
+        <img
+          class="mt-[27px] mx-auto mb-[22px]"
+          src="./assets/img/premium-quality 1.png"
+          alt=""
+        />
+        <p class="mb-[20px] text-center font-bold">24/7 support</p>
+        <p class="text-[12px] text-center">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto minus
+          facilis magni autem?
+        </p>
+      </div>
+      <div
+        class="
+          shadow-[0px_0px_25px_rgba(0,_0,_0,_0.1)]
+          sm:px-[12px]
+          px-[50px]
+          py-[20px]
+        "
+      >
+        <img
+          class="mt-[27px] mx-auto mb-[22px]"
+          src="./assets/img/24-hours-support 1.png"
+          alt=""
+        />
+        <p class="mb-[20px] text-center font-bold">24/7 support</p>
+        <p class="text-[12px] text-center">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto minus
+          facilis magni autem?
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <!-- latest trending products -->
+  <section class="mt-20 bg-[#F1F0FF]">
+    <div class="container sm:flex mx-auto px-[30px]">
+      <div><img src="./assets/img/Group 153.png" alt="" /></div>
+      <div class="">
+        <p class="text-[35px] sm:mt-[82px] text-[#151875]">
+          Unique Features Of leatest & Trending Poducts
+        </p>
+        <div class="flex pt-[29px] pb-[13px] space-x-3">
+          <img src="./assets/img/Ellipse 65.svg" alt="" />
+          <p>All frames constructed with hardwood solids and laminates</p>
+        </div>
+        <div class="flex pb-[13px] space-x-3">
+          <img src="./assets/img/Ellipse 66.svg" alt="" />
+          <p>
+            Reinforced with double wood dowels, glue, screw - nails corner
+            blocks and machine nails
+          </p>
+        </div>
+        <div class="flex mb-[38px] space-x-3">
+          <img src="./assets/img/Ellipse 67.svg" alt="" />
+          <p>Arms, backs and seats are structurally reinforced</p>
+        </div>
+        <div class="flex items-center space-x-[19px]">
+          <button class="bg-[#FB2E86] text-white py-[14px] px-[24px]">
+            Add to Cart
+          </button>
+          <div class="text-[#151875] text-[14px]">
+            <p>B&B Italian Sofa</p>
+            <p>$32.00</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div>
+      <div></div>
+      <div></div>
+    </div>
   </section>
 </template>
-
 <style>
 /* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;x

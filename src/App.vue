f91f4dca99d32122e7e069da2841c3bb4aed06b3
SalimKobi <salimkobi9@gmail.com>
2022-03-25 02:12:59 +0100

    made the product details page responsive

--- src/App.vue
+++ src/App.vue
@@ -113,11 +113,11 @@
   </section>
 
   <!-- products site info -->
-  <section>
-    <div class="flex">
-      <div><img src="./assets/img/Rectangle 32.png" alt=""></div>
-      <div>
-        <p>Dictum morbi</p>
+  <section class="mt-[4rem]">
+    <div class="sm:flex py-[30px] sm:px-[20px] sm:space-x-[41px] mx-auto items-center shadow-[0px_0px_25px_rgba(0,_0,_0,_0.1)] basis-1 w-fit">
+      <div class="sm:mx-auto flex justify-center"><img src="./assets/img/Rectangle 32.png" alt=""></div>
+      <div class="space-y-[13px] px-[30px]">
+        <p class="text-[25px] pt-[20px] sm:pt-[0px] sm:text-[36px] text-[#0D134E]">Dictum morbi</p>
         <div class="flex space-x-[5px]">
           <div class="flex items-center">
           <img src="./assets/img/fille_star.svg" alt="">
@@ -132,23 +132,141 @@
         </div>
         <div class="flex space-x-[18px]">
           <p class="text-[]">$32.00</p>
-          <p class=" line-through text-[##FB2E86] ">$32.00</p>
+          <p class=" line-through text-[#FB2E86] ">$32.00</p>
         </div>
         <div>
-          <p>color</p>
+          <p class="font-bold">Color</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum soluta natus sint sunt! Accusamus, velit.</p>
         </div>
-        <div class="flex ">
+        <div class="flex space-x-[20px]">
           <button>Add to cart</button>
           <img src="./assets/img/uil_heart-alt.svg" alt="">
+        </div>
+        <div>
+          <p>Categories:</p>
+          <p>Tags</p>
+          <div class="flex space-x-[16px]">
+            <p>Share</p>
+            <div class="flex space-x-[10px] items-center">
+            <img src="./assets/img/twitter.svg" alt="">
+            <img src="./assets/img/insta.svg" alt="">
+            <img src="./assets/img/facebook.svg" alt="">
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </section>
 
   <!-- products info -->
-  <section>
+  <section class="bg-[#F9F8FE] mt-[131px]">
+    <div class="sm:flex justify-center container mx-auto pt-[107px] pb-[50px] sm:pb-[122px] sm:px-36">
+      <div class="">
+      <div class="pb-[61px]">
+        <ul class="px-3 sm:px-[0px] flex flex-wrap space-x-3 sm:space-x-[85px] text-[#151875] text-[20px] sm:text-[24px]">
+          <li class="underline ring-offset-8">Description</li>
+          <li>Additional Info</li>
+          <li>Reviews</li>
+          <li>Video</li>
+        </ul>
+      </div>
+      <div class="pb-[36px] px-[20px] sm:px-[0px]">
+        <p class="text-[22px] text-[#151875] pb-[10px]">Various tempor</p>
+        <p class="leading-[29px] text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quis, recusandae eius cum pariatur maiores explicabo eligendi, blanditiis non quos nam ipsum necessitatibus, deserunt hic delectus possimus! Dignissimos repellat a nostrum esse blanditiis, neque eveniet deleniti eaque asperiores velit quasi, minus at quo ipsum molestias? Saepe voluptatem ipsa voluptatibus cumque rerum voluptate blanditiis numquam unde soluta totam vero esse repellendus neque fugit, ab nemo accusamus optio.</p>
+      </div>
+      <div class="space-y-[14px] px-[10px] sm:px-[0px]">
+        <p class="text-[22px] px-[5px] sm:px-[0px] text-[#151875]">More details</p>
+        <div class="flex space-x-[12px]">
+          <img class="" src="./assets/img/pointed_arrow.svg" alt="">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum aliquam a nihil perferendis odio! Quis harum iure provident, corporis eveniet consequatur ullam cum a.</p>
+        </div>
+        <div class="flex space-x-[12px]">
+          <img class="" src="./assets/img/pointed_arrow.svg" alt="">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum aliquam a nihil perferendis odio! Quis harum iure provident, corporis eveniet consequatur ullam cum a.</p>
+        </div>
+        <div class="flex space-x-[12px]">
+          <img class="" src="./assets/img/pointed_arrow.svg" alt="">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum aliquam a nihil perferendis odio! Quis harum iure provident, corporis eveniet consequatur ullam cum a.</p>
+        </div>
+        <div class="flex space-x-[12px]">
+          <img class="" src="./assets/img/pointed_arrow.svg" alt="">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum aliquam a nihil perferendis odio! Quis harum iure provident, corporis eveniet consequatur ullam cum a.</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
 
+  <section>
+    <div class="flex justify-center container mx-auto px-[36px]">
+      <div>
+        <p class="text-[25px] sm:text-[36px] text-center sm:text-left text-[#101750] pt-[50px] sm:pt-[126px]">Related products</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 md:max-w-[70vw] py-[30px] px-[30px] sm:px-[0px] container mx-auto">
+          <div class="">
+            <div><img src="./assets/img/Rectangle 128.png" alt=""></div>
+            <div class="flex space-x-[28px] sm:space-x-[60px]">
+              <p>Mens Fashion Wear</p>
+              <div class="flex space-x-[2px] items-center">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/empty_star.svg" alt="">
+              </div>
+            </div>
+            <p>$43.00</p>
+          </div>
+          <div class="">
+            <div><img src="./assets/img/Rectangle 133.svg" alt=""></div>
+            <div class="flex space-x-[28px] sm:space-x-[60px]">
+              <p>Mens Fashion Wear</p>
+              <div class="flex items-center">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/fille_star.svg" alt="">
+              </div>
+            </div>
+            <p>$67.00</p>
+          </div>
+          <div class="">
+            <div><img src="./assets/img/Rectangle 130.svg" alt=""></div>
+            <div class="flex space-x-[28px] sm:space-x-[60px]">
+              <p>Mens Fashion Wear</p>
+              <div class="flex items-center space-x-[2px]">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/empty_star.svg" alt="">
+              </div>
+            </div>
+            <p>$67.00</p>
+          </div>
+          <div class="">
+            <div><img src="./assets/img/Rectangle 131.png" alt=""></div>
+            <div class="flex space-x-[28px] sm:space-x-[60px]">
+              <p>Mens Fashion Wear</p>
+              <div class="flex items-center space-x-[2px]">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/fille_star.svg" alt="">
+              <img src="./assets/img/empty_star.svg" alt="">
+              <img src="./assets/img/empty_star.svg" alt="">
+              </div>
+            </div>
+            <p class="">$51.00</p>
+          </div>
+      </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="mt-[50px] sm:pt-[70px]">
+      <div class="flex justify-center container mx-auto">
+        <img class="text-center" src="./assets/img/image 1174.png" alt="">
+      </div>
   </section>
   
   <footer class="mt-[50px] sm:mt-[115px] bg-[#EEEFFB] text-[#8A8FB9]">
